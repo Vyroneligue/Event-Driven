b105165cc602c826d7ea5279c5cda5f69fdf1063
DanimarYanuario <danimarpalamos16@gmail.com>
2021-08-28 09:44:15 +0800

    Added my gallery

---
+++ gallery/gallery.php
@@ -0,0 +1,63 @@
+<html>
+<head>
+<style>
+div.gallery {
+  margin: 10px;
+  border: 5px solid blue;
+  float: center;	
+  width: 150px;
+  background-color: white;
+}
+
+div.gallery:hover {
+  border: 5px solid blue;
+}
+
+div.gallery img {
+  width: 100%;
+  height: 200px;
+}
+
+div.desc {
+  padding: 20px;
+  text-align: center;
+}
+</style>
+
+<style>
+body {
+	background-color: skyblue;
+	background-repeat: no-repeat;
+	background-size: cover;
+}
+</style>
+
+</head>
+<body>
+
+<div class="header">
+	<center><header><h1>GALLERY</h1><header></center>
+</div>
+
+<div class="gallery">
+    <img src="p1.jpg" alt="" width="600" height="400">
+  <div class="desc">Solo Fight</div>
+</div>
+
+<div class="gallery">
+    <img src="p2.jpg" alt="" width="600" height="400">
+  <div class="desc">Basketball Lover</div>
+</div>
+
+<div class="gallery">
+    <img src="p3.jpg" alt="" width="600" height="400">
+  <div class="desc">Classmate </div>
+</div>
+
+<div class="gallery">
+    <img src="p4.jpg" alt="" width="600" height="400">
+  <div class="desc">Grade 12</div>
+</div>
+
+</body>
+</html>
