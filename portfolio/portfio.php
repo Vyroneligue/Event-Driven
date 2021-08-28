38014fd35837c45fd50c066a463f2fa070646174
DanimarYanuario <danimarpalamos16@gmail.com>
2021-08-28 10:03:24 +0800

    Added my gallery

---
+++ portfolio/portfio.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>My Portfio</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+	<style>
+body {
+	background-image: url("dash.jpg");
+	background-repeat: no-repeat;
+	background-size: cover;
+}
+</style>
+
+</head>
+<body>
+
+<div class="header-a">
+	<header>VYRON ELIGUE</header>
+</div>
+
+<div class="link-a">
+	<a href="facebook.php">My Facebook</a></p>
+	<a href="about.php">About Me</a></p>
+</div>
+
+</body>
+</html>
