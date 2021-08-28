9ea2bedd4310a56fb7dfc9a9f04e550044b10f17
DanimarYanuario <danimarpalamos16@gmail.com>
2021-08-28 09:29:55 +0800

    Added my portfolio

---
+++ portfolio/about.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About Me</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+<style>
+body {
+	background-image: url("my.jpg");
+	background-repeat: no-repeat;
+	background-size: cover;
+}
+</style>
+
+<div class="link-b">
+<p><a href="portfio.php">Back</a></p>
+</div>
+
+</head>
+<body>
+
+<div class="p-a">
+	<p>My name is Vyron Eligue but you can call me Vy for short, I am 21 years of age and I live in Tanauan, Leyte if you need me call this number 09983363950</p>
+	<p>My hobby is playing Basketball, Volleyball, Photo Editing and Swimming in the beach every sunday after the mass.</p>
+</div>
+
+</body>
+</html>
